7e18f7322657a94cca38a8d650387c1e5cf87e5f
rshankarj <rshankarj@gmail.com>
2025-02-25 07:15:42 +0530

    my first commit

---
+++ src/main/java/com/demo/secureapp_jwt/SecureappJwtApplication.java
@@ -0,0 +1,13 @@
+package com.demo.secureapp_jwt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureappJwtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecureappJwtApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/demo/secureapp_jwt/SecureappJwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.secureapp_jwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecureappJwtApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
